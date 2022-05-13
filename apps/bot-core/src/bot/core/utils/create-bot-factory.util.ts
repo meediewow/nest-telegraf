4fc7c24d241dd918fdb3840f7bfd8b0738ab0a83
Mikhail Didevich <mididevich@gmail.com>
2022-05-13 17:31:10 +0300

    Added catch all errors

--- apps/bot-core/src/bot/core/utils/create-bot-factory.util.ts
+++ apps/bot-core/src/bot/core/utils/create-bot-factory.util.ts
@@ -1,5 +1,7 @@
+import { Logger } from '@nestjs/common';
 import { Telegraf } from 'telegraf';
 import { TelegrafModuleOptions } from '../interfaces/telegraf-options.interface';
+import * as Sentry from '@sentry/node';
 
 export async function createBotFactory(
   options: TelegrafModuleOptions,
@@ -11,6 +13,11 @@ export async function createBotFactory(
   if (options.launchOptions !== false) {
     await bot.launch(options.launchOptions);
   }
+
+  bot.catch((err) => {
+    Logger.error(err);
+    Sentry.captureException(err);
+  });
 
   return bot;
 }
