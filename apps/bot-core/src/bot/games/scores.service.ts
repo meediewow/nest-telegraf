8cdf103f14c2c8a213e8cbd9eeb6362ee835df4e
Mikhail Didevich <mididevich@gmail.com>
2022-05-15 14:20:05 +0300

    records as one message

--- apps/bot-core/src/bot/games/scores.service.ts
+++ apps/bot-core/src/bot/games/scores.service.ts
@@ -27,41 +27,39 @@ export class ScoresService {
     const reply = (message: string) => {
       return ctx.reply(message, { parse_mode: 'Markdown' });
     };
+    let result: string[] = [];
     games.forEach(async (i) => {
       switch (i.game) {
         case Games.Archeology: {
-          if (!i.top) {
-            await reply('Рекорд археологии не установлен');
-            break;
+          if (i.top) {
+            result = [
+              ...result,
+              `${i.top.username} стал лучшим археологом, выкопав ${i.top.item}. Возраст артефакта ${i.top.weight} лет.`,
+            ];
           }
-
-          await reply(
-            `${i.top.username} стал лучшим археологом, выкопав ${i.top.item}. Возраст артефакта ${i.top.weight} лет.`,
-          );
           break;
         }
         case Games.Hunt: {
-          if (!i.top) {
-            await reply('Рекорд охоты не установлен');
-            break;
+          if (i.top) {
+            result = [
+              ...result,
+              `${i.top.username} стал обладателем рекорда по охоте, добыв ${i.top.item}, весом в ${i.top.weight} кило.`,
+            ];
           }
-          await reply(
-            `${i.top.username} стал обладателем рекорда по охоте, добыв ${i.top.item}, весом в ${i.top.weight} кило.`,
-          );
           break;
         }
         case Games.Fish: {
-          if (!i.top) {
-            await reply('Рекорд рыбалки не установлен');
-            break;
+          if (i.top) {
+            result = [
+              ...result,
+              `${i.top.username} удерживает рекорд по рыбалке, поймав ${i.top.item}, весом в ${i.top.weight} кило.`,
+            ];
           }
-          await reply(
-            `${i.top.username} удерживает рекорд по рыбалке, поймав ${i.top.item}, весом в ${i.top.weight} кило.`,
-          );
           break;
         }
       }
     });
+    reply(result.join('\n\n'));
   }
 
   listenEvents() {
